73408a5f636fa5ab45f4f6b1092c310dc2438167
atomar94 <atomar94@ucla.edu>
2017-07-09 12:47:43 -0700

    directory creation works

--- main.py
+++ main.py
@@ -6,61 +6,6 @@ www.youtube.com/watch?v=PT2_F-1esPk
 https://www.youtube.com/watch?v=ek7cafqgYB4
 https://www.youtube.com/watch?v=HQnC1UHBvWA
 https://www.youtube.com/watch?v=s8XIgR5OGJc
-https://www.youtube.com/watch?v=lEi_XBg2Fpk
-https://www.youtube.com/watch?v=a52Ul2AM92c
-https://www.youtube.com/watch?v=ULHeRdgeT54
-https://www.youtube.com/watch?v=5JxgDJvqGmM
-https://www.youtube.com/watch?v=2pY_WobtVuQ
-https://www.youtube.com/watch?v=ALZHF5UqnU4
-https://www.youtube.com/watch?v=tD4HCZe-tew
-https://www.youtube.com/watch?v=qDRORgoZxZU
-https://www.youtube.com/watch?v=si81bIoZRJQ
-https://www.youtube.com/watch?v=AeGfss2vsZg
-https://www.youtube.com/watch?v=wvzfOyW0ZMo
-https://www.youtube.com/watch?v=SMs0GnYze34
-https://www.youtube.com/watch?v=BPzG12uXSwQ
-https://www.youtube.com/watch?v=85ftfVUTzM4
-https://www.youtube.com/watch?v=Trzzg6qL7Ic
-https://www.youtube.com/watch?v=BC_Ya4cY8RQ
-https://www.youtube.com/watch?v=PZvFFezMTOU
-https://www.youtube.com/watch?v=uW__O0QruV4
-https://www.youtube.com/watch?v=mOKqNxN4jWM
-https://www.youtube.com/watch?v=-WlAJxk8OhU
-https://www.youtube.com/watch?v=Po7e5jD18l0
-https://www.youtube.com/watch?v=a59gmGkq_pw
-https://www.youtube.com/watch?v=sO9cBXRcBvo
-https://www.youtube.com/watch?v=K_yBUfMGvzc
-https://www.youtube.com/watch?v=ANIkOH0Hb9Y
-https://www.youtube.com/watch?v=xAIoh9rxRi8
-https://www.youtube.com/watch?v=Nng_zGDhHgg
-https://www.youtube.com/watch?v=Nng_zGDhHgg
-https://www.youtube.com/watch?v=hgKDu5pp_fU
-https://www.youtube.com/watch?v=iob9UYFwFwk
-https://www.youtube.com/watch?v=Io0fBr1XBUA
-https://www.youtube.com/watch?v=XPzCzaBhF7s
-https://www.youtube.com/watch?v=u3VFzuUiTGw
-https://www.youtube.com/watch?v=pnSZbl8fTHM
-https://www.youtube.com/watch?v=AUU7xY7R5s0
-https://www.youtube.com/watch?v=B2m_WnXjqnM
-https://www.youtube.com/watch?v=rTlqY9i7Gn4
-https://www.youtube.com/watch?v=dMMUH_ZpbB0
-https://www.youtube.com/watch?v=Ly7uj0JwgKg
-https://www.youtube.com/watch?v=SXiSVQZLje8
-https://www.youtube.com/watch?v=MMaXUVyJBos
-https://www.youtube.com/watch?v=SgO9yYlS9QY
-https://www.youtube.com/watch?v=aH9eOTGE-Es
-https://www.youtube.com/watch?v=mgEixhE3Oms
-https://www.youtube.com/watch?v=PyfYvLeG0tc
-https://www.youtube.com/watch?v=P3CxhBIrBho
-https://www.youtube.com/watch?v=eC-F_VZ2T1c
-https://www.youtube.com/watch?v=juiq5YZlw0U
-https://www.youtube.com/watch?v=lY2yjAdbvdQ
-https://www.youtube.com/watch?v=jsbeemdD2rQ
-https://www.youtube.com/watch?v=jGflUbPQfW8&list=RDjGflUbPQfW8#t=9
-https://www.youtube.com/watch?v=YqeW9_5kURI&index=2&list=RDjGflUbPQfW8
-https://www.youtube.com/watch?v=fRh_vgS2dFE&index=5&list=RDjGflUbPQfW8
-https://www.youtube.com/watch?v=7PCkvCPvDXk&list=RDjGflUbPQfW8&index=16
-https://www.youtube.com/watch?v=euCqAq6BRa4&index=20&list=RDjGflUbPQfW8
 """
 #songs="https://www.youtube.com/watch?v=otv7lJmLJtQ"
 #unsure of their artist/title
@@ -72,7 +17,14 @@ _verbose = True
 def print_v(s):
     if _verbose:
         print(s)
-def get(songurl):
+
+_debug = True
+def print_d(s):
+    if _debug:
+        print(s)
+
+#return a tuple/3 (yt object, artist, title)
+def getter(songurl):
 
     # Sanitize
     if not songurl.strip():
@@ -84,39 +36,63 @@ def get(songurl):
     artist = ""
     title = ""
     yt = None
-
     try:
         yt = pafy.new(songurl)
     except ValueError:
         print_v("Pafy can't parse the url: " + songurl)
         return
+
     if len(yt.title.split('-')) == 2:
         artist = yt.title.split('-')[0].strip()
         title = yt.title.split('-')[1].strip()
     else:
         unsure_songs.append(songurl)
         print_v("Could not parse artist/title from " + yt.title)
-        return
+        artist = "Could Not Parse"
+        title = yt.title.strip()
+    return {"yt": yt, 
+            "artist": artist, 
+            "title": title}
 
-    path = root + artist
-    if not os.path.exists(path):
-        print_v("making dir for <" + artist + ">")
-        os.makedirs(path, exist_ok=True)
-    try:
-        bestaudio = yt.getbestaudio(preftype="m4a")
-        bestaudio.download(filepath=path+ "/" + title + "." + bestaudio.extension, quiet=True)
-    except FileExistsError:
-        print_v("You already have <" + title + ">")
-    except FileNotFoundError:
-        print_v("error with <" + title + ">")
-        unsure_songs.append(songurl)
+def directory_constructor(metadatas):
+    paths = []
+    for m in metadatas:
+        print_d(type(m))
+        m['path'] = root + m['artist']
+        paths.append(m['path'])
+
+    print("Do you want to create these directories?")
+    for p in paths:
+        print(p)
+
+    if input("[Y/n]").lower().strip() == "y":
+        for m in metadatas:
+            if not os.path.exists(m['path']):
+                print_v("making dir for <" + m['artist'] + ">")
+                os.makedirs(m['path'], exist_ok=True)
+            try:
+                bestaudio = m['yt'].getbestaudio(preftype="m4a")
+                bestaudio.download(filepath=m['path'] + "/" + m['title'] + "." + bestaudio.extension, quiet=True)
+            except FileExistsError:
+                print_v("You already have <" + m['title'] + ">")
+            except FileNotFoundError:
+                print_v("error with <" + m['title'] + ">")
+                unsure_songs.append(m['yt'].url)
 
 
-for songurl in songs.split("\n"):
-    get(songurl)
+song_data = []
+
+for i, songurl in enumerate(songs.split("\n")):
+    print_v("Parsing " + str(i) + " of " + str(len(songs.split("\n"))))
+    metadata = getter(songurl)
+    if metadata:
+        song_data.append(metadata)
+
+directory_constructor(song_data)
+
     
 while True:
     try:
-        get(input("Song URL: "))
+        getter(input("Song URL: "))
     except:
-        print_v("There was an error")
+        exit()
